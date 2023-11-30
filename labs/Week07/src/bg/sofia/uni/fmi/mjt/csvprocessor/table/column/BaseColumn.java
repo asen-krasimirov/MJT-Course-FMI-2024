f79bb2c4b3766d5ec924c14a925686eae67b03ad
asen-krasimirov <personalgrowth57@gmail.com>
2023-11-30 23:52:00 +0200

    implementing BaseColumn and BaseColumnTest

---
+++ labs/Week07/src/bg/sofia/uni/fmi/mjt/csvprocessor/table/column/BaseColumn.java
@@ -0,0 +1,34 @@
+package bg.sofia.uni.fmi.mjt.csvprocessor.table.column;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class BaseColumn implements Column {
+
+    private final Set<String> values;
+
+    public BaseColumn() {
+        this(new LinkedHashSet<>());
+    }
+
+    public BaseColumn(Set<String> values) {
+        this.values = values;
+    }
+
+    @Override
+    public void addData(String data) {
+        if (data == null || data.isBlank() || data.isEmpty()) {
+            throw new IllegalArgumentException("Value of data should not be null or blank!");
+        }
+
+        values.add(data);
+    }
+
+    @Override
+    public Collection<String> getData() {
+        return Collections.unmodifiableCollection(values);
+    }
+
+}
---
+++ labs/Week07/test/bg/sofia/uni/fmi/mjt/csvprocessor/table/column/BaseColumnTest.java
@@ -0,0 +1,64 @@
+package bg.sofia.uni.fmi.mjt.csvprocessor.table.column;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class BaseColumnTest {
+
+    BaseColumn baseColumn;
+
+    @BeforeEach
+    void setUp() {
+        baseColumn = new BaseColumn();
+    }
+
+    @Test
+    void testAddNullData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> baseColumn.addData(null),
+            "addData(...) should throw IllegalArgumentException when called with null value.");
+    }
+
+    @Test
+    void testAddBlankData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> baseColumn.addData("   "),
+            "addData(...) should throw IllegalArgumentException when called with blank string.");
+    }
+
+    @Test
+    void testAddEmptyData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> baseColumn.addData(""),
+            "addData(...) should throw IllegalArgumentException when called with empty string.");
+    }
+
+    @Test
+    void testAddCorrectData() {
+        Assertions.assertDoesNotThrow(() -> baseColumn.addData("testString"),
+            "addData(...) should not throw when is called with correct data.");
+    }
+
+    @Test
+    void testGetData() {
+        baseColumn.addData("test1");
+        baseColumn.addData("test2");
+        baseColumn.addData("test3");
+
+        Collection<String> expectedResult = new ArrayList<>();
+        expectedResult.add("test1");
+        expectedResult.add("test2");
+        expectedResult.add("test3");
+
+        Collection<String> result = baseColumn.getData();
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertIterableEquals(expectedResult, result, "getData(...) should return correct date then called.");
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> result.add("test4"),
+            "getData(...) result should be able to be modified.");
+    }
+
+}
